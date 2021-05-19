aa426a78f98d70fd44eaacdbb53767b44eead1b3
hancehardy <hthardy@gmail.com>
2021-05-18 21:00:41 -0700

    added Bootstrap 5 and Navbar

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,7 @@
 class Contract < ApplicationRecord
+    belongs_to :salesmen
+    belongs_to :referrals
+
     validates :job_number, presence: true
     validates :job_name, presence: true
     validates :job_amount, presence: true
--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,2 +1,3 @@
 class Referral < ApplicationRecord
+    has_many :contracts
 end
