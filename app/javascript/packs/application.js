26c589e75feecde2f08c2427580ee1e60348e3b5
hancehardy <hthardy@gmail.com>
2021-05-21 23:16:22 -0700

    @babel error fixed??

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,16 +5,16 @@
 
 
 
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "bootstrap"
+
 
 
