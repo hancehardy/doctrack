9b0a1900382136dd99e2690ce4513da7f5eda076
hancehardy <hthardy@gmail.com>
2021-05-27 23:20:08 -0700

    Changed to MYSQL from SQLITE. Added some formatting.

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -8,7 +8,15 @@ class Contract < ApplicationRecord
     validates :job_name, presence: true
     validates :job_amount, presence: true
     validates :milestone_1_date, presence: true
+
+
+    def salesman_commission
+        return self.job_amount * self.salesman.sales_commission        
+    end
         
+    def salesman_commission_formatted
+        return ActionController::Base.helpers.number_to_currency(self.job_amount * self.salesman.sales_commission)
+    end
     
     
    
